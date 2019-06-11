378cb26f3fa8bd413545b6e8fc103f42765efc7e
Melanie Rogan <melanie.rogan@ft.com>
2019-06-11 16:35:31 +0100

    airport landing and take off and tests

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,8 +1,29 @@
 describe('airport', function() {
   it('can land a plane', function(){
     airport = new Airport
-    plane = "this is a plane"
+    plane = { isFlying: true }
     airport.land(plane)
     expect(airport.hangar).toContain(plane)
   })
+
+  it('cannot land plane that is not flying', function(){
+    airport = new Airport
+    plane = { isFlying: false }
+    expect(function () { airport.land(plane) }).toThrow('No!')
+  })
+
+  it('plane can takeoff from airport', function() {
+    airport = new Airport
+    plane = { isFlying: true}
+    airport.land(plane)
+    plane.isFlying = false 
+    airport.takeOff(plane)
+    expect(airport.hangar).toEqual([])
+  })
+
+  it('a plane cannot take off if it has already taken off', function() {
+    airport = new Airport
+    plane = { isFlying: true }
+    expect(function () { airport.takeOff(plane) }).toThrow('Plane has already taken off')
+  });
 });
--- src/airport.js
+++ src/airport.js
@@ -3,5 +3,19 @@ function Airport() {
 }
 
 Airport.prototype.land = function(plane) {
-  this.hangar.push(plane)
+  if (plane.isFlying === true)
+    this.hangar.push(plane)
+  else
+    throw('No!')
 }
+
+Airport.prototype.takeOff = function(plane) {
+  if (plane.isFlying) throw('Plane has already taken off')
+  var result = this.hangar.filter(function(hangarPlane) { 
+    if (plane === hangarPlane)
+      return false
+    else 
+      return true
+  })
+  this.hangar = result
+};
