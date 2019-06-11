718384a683d8c704b7a3aac04c2ce086deed5be0
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-06-11 17:08:58 +0100

    airport capacity - this went well

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,20 +1,20 @@
 describe('airport', function() {
   it('can land a plane', function(){
-    airport = new Airport
-    plane = { isFlying: true }
+    var airport = new Airport
+    var plane = { isFlying: true }
     airport.land(plane)
     expect(airport.hangar).toContain(plane)
   })
 
   it('cannot land plane that is not flying', function(){
-    airport = new Airport
-    plane = { isFlying: false }
+    var airport = new Airport
+    var plane = { isFlying: false }
     expect(function () { airport.land(plane) }).toThrow('No!')
   })
 
   it('plane can takeoff from airport', function() {
-    airport = new Airport
-    plane = { isFlying: true}
+    var airport = new Airport
+    var plane = { isFlying: true }
     airport.land(plane)
     plane.isFlying = false 
     airport.takeOff(plane)
@@ -22,8 +22,20 @@ describe('airport', function() {
   })
 
   it('a plane cannot take off if it has already taken off', function() {
-    airport = new Airport
-    plane = { isFlying: true }
+    var airport = new Airport
+    var plane = { isFlying: true }
     expect(function () { airport.takeOff(plane) }).toThrow('Plane has already taken off')
   });
+
+  it('a plane cannot land if the airport is full', function() {
+    var airport = new Airport
+    for(i = 0; i < airport.capacity; i++) {
+       eval(`var plane${i} = { isFlying: true }`)
+       eval(`airport.land(plane${i})`)
+    }
+    console.log(airport.hangar)
+    var strandedPlane = { isFlying: true }
+    expect(function() { airport.land(strandedPlane) }).toThrow('Airport is full')
+
+  })
 });
--- src/airport.js
+++ src/airport.js
@@ -1,9 +1,11 @@
 function Airport() {
   this.hangar = []
+  this.capacity = 5
 }
 
 Airport.prototype.land = function(plane) {
-  if (plane.isFlying === true)
+  if (this.hangar.length >= this.capacity) throw('Airport is full')
+  else if (plane.isFlying === true)
     this.hangar.push(plane)
   else
     throw('No!')
